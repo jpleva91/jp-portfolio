a3510bddf151d7a2e29f9dc0cbcdbae2c84d6795
jpleva91 <jpleva91@gmail.com>
2025-06-27 08:05:30 -0400

    Fix paths for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   base: '/jp-portfolio/',
-  plugins: [
-    legacy({
-      targets: ['defaults', 'not IE 11']
-    })
-  ],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
